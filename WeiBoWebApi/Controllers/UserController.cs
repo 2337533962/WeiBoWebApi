423156bbfb40ddda086b47d2a2c71a1b13a3959d
YeGuangJin <2337533962@qq.com>
2022-03-05 16:12:20 +0800

    新增一些User接口

--- WeiBoWebApi/Controllers/UserController.cs
+++ WeiBoWebApi/Controllers/UserController.cs
@@ -41,6 +41,61 @@ namespace WeiBoWebApi.Controllers
 
         }
 
+        /// <summary>
+        /// 获取所有性别信息
+        /// </summary>
+        [HttpGet("/sex/all")]
+        public IEnumerable<SexInfo> GetAllSexInfo()
+        {
+            return new SexInfoBll().GetAllModel();
+        }
+
+        /// <summary>
+        /// 根据性别id获取性别值
+        /// </summary>
+        [HttpGet("/sex")]
+        public object GetSexBySexId(int sexId) {
+            return new SexInfoBll().GetModel(sexId);
+        }
+
+        /// <summary>
+        /// 根据用户Id获取ta关注的信息
+        /// </summary>
+        [HttpGet("/follow/list")]
+        public IEnumerable<AttentionInfo> GetAttentionInfos(int uid) {
+            return new AttentionInfoBll().GetModelList("followId=@uid", 
+                new System.Data.SqlClient.SqlParameter("uid", uid));
+        }
+
+        /// <summary>
+        /// 根据用户Id获取ta关注的数量
+        /// </summary>
+        [HttpGet("/follow/count")]
+        public int GetFollowNum(int uid)
+        {
+            return new AttentionInfoBll().GetModelList("followId=@uid",
+                new System.Data.SqlClient.SqlParameter("uid", uid)).Count;
+        }
+
+        /// <summary>
+        /// 根据用户Id获取ta粉丝的信息
+        /// </summary>
+        [HttpGet("/fans/list")]
+        public IEnumerable<AttentionInfo> GetFansList(int uid)
+        {
+            return new AttentionInfoBll().GetModelList("followToId=@uid",
+                new System.Data.SqlClient.SqlParameter("uid", uid));
+        }
+
+        /// <summary>
+        /// 根据用户Id获取ta粉丝的数量
+        /// </summary>
+        [HttpGet("/fans/count")]
+        public int GetFansCount(int uid)
+        {
+            return new AttentionInfoBll().GetModelList("followToId=@uid",
+                new System.Data.SqlClient.SqlParameter("uid", uid)).Count;
+        }
 
     }
 }
