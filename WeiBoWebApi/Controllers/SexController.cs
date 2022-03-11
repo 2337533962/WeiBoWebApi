4e38254ed97b51422d145a7d4242e3d5b71acd62
YeGuangJin <2337533962@qq.com>
2022-03-11 17:41:41 +0800

    保持基本可用

---
+++ WeiBoWebApi/Controllers/SexController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeiBoWebApi.BLL;
+using WeiBoWebApi.Model;
+
+namespace WeiBoWebApi.Controllers
+{
+    /// <summary>
+    /// 性别
+    /// </summary>
+    [Route("[controller]")]
+    [ApiController]
+    public class SexController : ControllerBase
+    {
+        /// <summary>
+        /// 获取所有性别信息
+        /// </summary>
+        [HttpGet("/sex/all")]
+        public IEnumerable<SexInfo> GetAllSexInfo()
+        {
+            return new SexInfoBll().GetAllModel();
+        }
+        /// <summary>
+        /// 根据性别id获取性别值
+        /// </summary>
+        [HttpGet("/sex")]
+        public object GetSexBySexId(int sexId)
+        {
+            return new SexInfoBll().GetModel(sexId);
+        }
+    }
+}
---
+++ WeiBoWebApi/ViewModel/UserBasicInfo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeiBoWebApi.ViewModel
+{
+    /// <summary>
+    /// 用户基本信息
+    /// </summary>
+    public class UserBasicInfo
+    {
+        /// <summary>
+        /// 头像
+        /// </summary>
+        public string HeadPortrait { get; set; }
+        /// <summary>
+        /// 账号
+        /// </summary>
+        public string Account { get; set; }
+        /// <summary>
+        /// 昵称
+        /// </summary>
+        public string NickName { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public string Sex { get; set; }
+        /// <summary>
+        /// 生日
+        /// </summary>
+        public DateTime? Birth { get; set; }
+        /// <summary>
+        /// 关注
+        /// </summary>
+        public int? Follow { get; set; }
+        /// <summary>
+        /// 粉丝
+        /// </summary>
+        public int? Fans { get; set; }
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// 令牌
+        /// </summary>
+        public string Token { get; set; }
+    }
+}
